11003c4335a2b4d95a08637d717e207c2f8753ae
Manish0085 <manishk57107@gmail.com>
2026-02-23 20:47:01 +0530

    Changing the origin

--- ChatVerse-Backend/src/main/java/com/substring/chat/security/WebSecurityConfig.java
+++ ChatVerse-Backend/src/main/java/com/substring/chat/security/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(Arrays.asList("https://chat-versa-zm6p.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
